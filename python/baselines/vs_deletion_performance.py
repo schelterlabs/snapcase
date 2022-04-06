062c89fd332d82e4eb6724f6cbbd34cc5cd5a973
sscdotopen <ssc.open@googlemail.com>
2022-04-06 13:43:49 +0200

    baselines

--- python/baselines/vs_deletion_performance.py
+++ python/baselines/vs_deletion_performance.py
@@ -63,18 +63,17 @@ def run_experiment(dataset, historical_sessions_file, evolving_sessions_file,
 
 num_queries_to_evaluate in [100, 1000, 10000]
 # Wish we could do more, but the experiment takes already too long
-num_clicks_to_delete = 10
+num_clicks_to_delete = 20
 
 for seed in [42, 767, 999]:
-    for num_queries_to_evaluate in [100, 1000, 10000]:
-        run_experiment('ecom1m', "../../datasets/session/bolcom-clicks-1m_train.txt",
-                       "../../datasets/session/bolcom-clicks-1m_test.txt",
-                       num_clicks_to_delete, num_queries_to_evaluate)
+    run_experiment('ecom1m', "../../datasets/session/bolcom-clicks-1m_train.txt",
+                   "../../datasets/session/bolcom-clicks-1m_test.txt",
+                   num_clicks_to_delete, num_queries_to_evaluate)
 
-        run_experiment('rsc15', "../../datasets/session/rsc15-clicks_train_full.txt",
-                       "../../datasets/session/rsc15-clicks_test.txt",
-                       num_clicks_to_delete, num_queries_to_evaluate)
+    run_experiment('rsc15', "../../datasets/session/rsc15-clicks_train_full.txt",
+                   "../../datasets/session/rsc15-clicks_test.txt",
+                   num_clicks_to_delete, num_queries_to_evaluate)
 
-        run_experiment('ecom60m', "../../datasets/session/bolcom-clicks-50m_train.txt",
-                        "../../datasets/session/bolcom-clicks-50m_test.txt",
-                        num_clicks_to_delete, num_queries_to_evaluate)
+    run_experiment('ecom60m', "../../datasets/session/bolcom-clicks-50m_train.txt",
+                    "../../datasets/session/bolcom-clicks-50m_test.txt",
+                    num_clicks_to_delete, num_queries_to_evaluate)
